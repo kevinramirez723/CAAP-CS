1f2c7045fe521c11218a6664cbdb30ed6b458691
Kevin Ramirez <kevinramirez723@gmail.com>
2018-07-07 19:50:31 -0500

    added loop function to drastically shorten code

--- assignment_01/change.py
+++ assignment_01/change.py
@@ -1,27 +1,23 @@
 #change
 
 def main():
-	change = 100*eval(input("How much change is owed? "))	#multipling user input (and just about everything else) by 100 to avoid floating point errors 
-	(d100, change) = divmod(change, 10000)					#divmod(x, y) -> (int div, mod)
-	(d50, change) = divmod(change, 5000)					#larger values are removed from user's change first
-	(d20, change) = divmod(change, 2000)					#and remainder is set as change for next step down
-	(d10, change) = divmod(change, 1000)
-	(d5, change) = divmod(change, 500)
-	(d1, change) = divmod(change, 100)
-	(q, change) = divmod(change, 25)
-	(d, change) = divmod(change, 10)
-	(n, change) = divmod(change, 5)
-	p = round(change)
-	total = d100+d50+d20+d10+d5+d1+q+d+n+p
-	print("$100 bills: ",d100)								#each integer divisor of change is then displayed in order of value
-	print("$50 bills: ",d50)
-	print("$20 bills: ",d20)
-	print("$10 bills: ",d10)
-	print("$5 bills: ",d5)
-	print("$1 bills: ",d1)
-	print("quarters: ",q)
-	print("dimes: ",d)
-	print("nickels: ",n)
-	print("pennies: ",p)
-	print("total: ",total)									#all previous variables added together
+	d100, d50, d20, d10, d5, d1, q, d, n = 0, 0, 0, 0, 0, 0, 0, 0, 0
+	units = [d100, d50, d20, d10, d5, d1, q, d, n] 			#one list for variables
+	values = [10000, 5000, 2000, 1000, 500, 100, 25, 10, 5]	#corresponding list of the worth of each unit(x100) to divide the change by
+	change = 100*eval(input("How much change is owed? "))	#multipling user input (and just about everything else) by 100 to avoid floating point errors
+	for i in range(9):										#divmod(x, y) -> (int div, mod)
+		(units[i], change) = divmod(change, values[i])		#larger values are removed from user's change first and stored as one of the variables in the first list									
+	total = sum(units)+round(change)						#remainder is set as change for next loop
+	print(
+	"$100 bills: ",units[0],"\n"							#units list is summed up and added with the change left over (pennies)
+	"$50 bills: ",units[1],"\n"
+	"$20 bills: ",units[2],"\n"
+	"$10 bills: ",units[3],"\n"
+	"$5 bills: ",units[4],"\n"
+	"$1 bills: ",units[5],"\n"
+	"quarters: ",units[6],"\n"
+	"dimes: ",units[7],"\n"
+	"nickels: ",units[8],"\n"
+	"pennies: ",round(change),"\n"
+	"total: ",total)									#all previous variables added together
 main()
