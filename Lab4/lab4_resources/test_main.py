98b71031f31cd972366947341e3f22bf072cbf79
Kevin Ramirez <kevinramirez723@gmail.com>
2018-08-04 23:04:30 -0500

    Lab 4

---
+++ Lab4/lab4_resources/test_main.py
@@ -0,0 +1,8 @@
+from nose import tools						# importing package to be able to run the tests
+from main import *							# Here you should import all the modules/classes you want to test
+from mock import patch
+import turtle
+
+# define a function like these for each class/module you test.
+# etc...
+
